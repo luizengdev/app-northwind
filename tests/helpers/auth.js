1e3b8abc51e1416d7d555eb7ec49729f7f377f55
luizengdev <luizengdev@gmail.com>
2026-09-01 23:28:21 -0300

    refactor: update login helper to use dynamic credentials from fixture

--- tests/helpers/auth.js
+++ tests/helpers/auth.js
@@ -1,9 +1,10 @@
 import {expect} from "@playwright/test";
+import loginData from "../../fixtures/login-data.json";
 
 export default async function loginAsAdmin(page) {
   await page.goto("/");
-  await page.getByTestId("email-input").fill("admin@qatest.com");
-  await page.getByTestId("password-input").fill("Teste@123");
+  await page.getByTestId("email-input").fill(loginData.validUser.email);
+  await page.getByTestId("password-input").fill(loginData.validUser.password);
   await page.getByTestId("login-button").click();
 
   await expect(page.getByRole("heading", {name: "QA Automation Shop"})).toBeVisible();
